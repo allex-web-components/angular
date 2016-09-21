c7d051e640d6b69610e5503003882c8ed36711fe
Veljko Popovic <v.s.popovic@gmail.com>
2016-09-21 11:09:07 +0200

    allexRange introduced to dist

--- dist/lib.js
+++ dist/lib.js
@@ -177,3 +177,20 @@ angular.module ('allex__web_angularcomponent', []);
 
 
 })(ALLEX.lib, angular.module('allex__web_angularcomponent'));
+//samo da te vidim
+(function (lib, module) {
+  'use strict';
+  function range (input, start, end, inc){
+    start = parseFloat(start);
+    end = parseFloat(end);
+    inc = isNaN(inc) ? 1 : parseFloat(inc);
+
+    for (var i = start; i < end; i+=inc){
+      input.push(i);
+    }
+    return input;
+  }
+
+  module.filter ('allexRange' , function () { return range; });
+
+})(ALLEX.lib, angular.module('allex__web_angularcomponent'));
